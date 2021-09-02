8b1e322fe58a702e43154f2eeecb96f266673733
christianangeles <christian.angeles@smoothstack.com>
2021-09-02 13:58:52 -0700

    add scan status check for harbor scanner

--- harbor_scanner.py
+++ harbor_scanner.py
@@ -1,4 +1,4 @@
-import requests
+import requests, time, sys, json
 
 ## Arguments needed from user ##
 userPass = ('', '')
@@ -17,7 +17,21 @@ scanInitResp = requests.post(urlScanInit, data={}, auth=userPass)
 
 ## Checks scanner status ##
 urlScanOverview = urlBase + projectSha + '?with_scan_overview=true'
-scanOverviewResp = requests.get(urlScanOverview, auth=userPass)
-scanOverviewResult = scanOverviewResp.json()['scan_overview']['application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0']
+scanStatus = 'Pending'
+maxApiCall = 5
 
-print(scanOverviewResult)
+while scanStatus != 'Success':
+    scanOverviewResp = requests.get(urlScanOverview, auth=userPass)
+    scanOverviewResult = scanOverviewResp.json()['scan_overview']['application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0']
+    scanStatus = scanOverviewResult['scan_status']
+    print(scanStatus)
+    if scanStatus == 'Success':
+        break
+    elif maxApiCall <= 0:
+        print('Reached maximum API calls')
+        sys.exit(-1)
+    else:
+        maxApiCall -= 1
+        time.sleep(4)
+
+print(json.dumps(scanOverviewResult['summary'], indent=4))

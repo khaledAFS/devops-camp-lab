2c2abd79bc4322de21110d43a8139c492ccf46ed
christianangeles <christian.angeles@smoothstack.com>
2021-09-02 11:59:12 -0700

    parameterized URI for api calls

--- harbor_scanner.py
+++ harbor_scanner.py
@@ -1,18 +1,23 @@
 import requests
 
+## Arguments needed from user ##
+userPass = ('', '')
 registry = 'harbor.dev.afsmtddso.com'
 projectName = 'devsecops-lab'
-
-# projectSha = 'sha256:909adc24a5bdae62298897e17db0ecf63de8b13c03bbc331f55d75fc84d8492a'
-projectSha = 'sha256:5e95e5eb8be4322e3b3652d737371705e56809ed8b307ad68ec59ddebaaf60e4'
 imageName = 'nginx'
-url = 'https://' + registry + '/api/v2.0/projects//' + projectName + '/repositories/' + imageName +'/artifacts/' + projectSha + '/scan'
-userPass = ('', '')
 
-scanInitResp = requests.post(url, data={}, auth=userPass)
+## Grab sha256 digest from Harbor project repository ##
+urlBase = 'https://' + registry + '/api/v2.0/projects/' + projectName + '/repositories/' + imageName + '/artifacts/'
+digestResp = requests.get(urlBase, auth=userPass)
+projectSha = digestResp.json()[0]['digest']
 
-urlScanOverview = 'https://harbor.dev.afsmtddso.com/api/v2.0/projects/devsecops-lab/repositories/nginx/artifacts/sha256:5e95e5eb8be4322e3b3652d737371705e56809ed8b307ad68ec59ddebaaf60e4?with_scan_overview=true'
-scanResultResp = requests.get(urlScanOverview, auth=userPass)
-scanOverview = scanResultResp.json()['scan_overview']['application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0']
+## Initialize image scanner ##
+urlScanInit = urlBase + projectSha + '/scan'
+scanInitResp = requests.post(urlScanInit, data={}, auth=userPass)
 
-print(scanOverview)
+## Checks scanner status ##
+urlScanOverview = urlBase + projectSha + '?with_scan_overview=true'
+scanOverviewResp = requests.get(urlScanOverview, auth=userPass)
+scanOverviewResult = scanOverviewResp.json()['scan_overview']['application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0']
+
+print(scanOverviewResult)

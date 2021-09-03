6e04b56400ce67bbcafd92e343d3d816d026839a
christianangeles <christian.angeles@smoothstack.com>
2021-09-03 09:07:12 -0700

    update scanner credential input

--- harbor_scanner.py
+++ harbor_scanner.py
@@ -1,16 +1,13 @@
-import requests, time, sys, json, getopt
+import requests, time, sys, json, getopt, os
 
 ## Arguments needed from user ##
 argList = sys.argv[1:]
 options = 'c:i:p:r:'
 
 arguments, values = getopt.getopt(argList, options)
-print(arguments)
-print(values)
 for currentArgument, currentValue in arguments:
     if currentArgument in ("-c"):
-        print(currentValue)
-        username, password = currentValue.split(':')
+        username, password = os.getenv(currentValue).split(':')
     elif currentArgument in ("-i"):
         imageName = currentValue
     elif currentArgument in ("-p"):
